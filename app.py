e4dd5260cb687024dc23617dd75b3a3bd3260543
Snehil <snehilsneh888@gmail.com>
2021-05-20 10:29:06 +0530

    Connected to mongodb

--- app.py
+++ app.py
@@ -13,3 +13,13 @@ app = Flask(__name__)
 @app.route('/',methods=['POST','GET'])
 def index():
     if request.method=='POST':
+        searchString=request.form['content'].replace(" ","")
+        #this will shorten the input content
+        try:
+            #will access data from the existing database if present.
+            dbConn = pymongo.MongoClient("mongodb://localhost:27017/")
+            db = dbConn['crawlerDB']
+            reviews = db[searchString].find({})
+            if reviews.count() > 0:
+                return render_template('results.html', reviews=reviews)
+
