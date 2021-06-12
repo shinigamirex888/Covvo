a1152cb92f3a475bcd40fdd233e7f3a7b7fd6f3c
Snehil <snehilsneh888@gmail.com>
2021-06-12 21:10:22 +0530

    Stream handler and Product highlight function done

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import pymongo as mg
 import plotly.graph_objects as go
 import json
 
-""" checking the database logs(logger)"""
+# checking the database logs(logger)
 
 DBlogger=logging.getLogger('Database')
 
@@ -29,6 +29,30 @@ file_handeler.setLevel(logging.ERROR)
 file_handeler.setFormatter(formatter)
 
 DBlogger.addHandler(file_handeler)
+
+#Stream Handler To Get Logs on Consol
+
+stream_handler=logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+DBlogger.addHandler(stream_handler)
+
+app=Flask(__name__)
+
+
+#Function to get product Hightlights!
+
+def get_product_highlights(box):
+    lst=[]
+    highlights=box.find_all('li',{'class':'_21Ahn-'})
+    for i in range(len(highlights)):
+        lst.append(highlights[i].text)
+    return lst
+
+
+
+
+
+
 
 
 
